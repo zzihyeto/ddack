d5825bb1d7d7d109dd66942a1d20ae03ed5a50bb
zzihyeto <ljh9055@gmail.com>
2022-03-16 20:05:15 +0900

    jh220316 db connect

--- ddack/WebContent/test.jsp
+++ ddack/WebContent/test.jsp
@@ -1,10 +1,12 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <%@page import="java.sql.SQLException"%>
+
 <%@page import="db.JDBCUtility"%>
 
 <%@page import="java.sql.Connection"%>
 
-<!DOCTYPE>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <html>
 
@@ -28,11 +30,13 @@
 
 			conn = JDBCUtility.getConnection();
 
+			conn.setAutoCommit(false);
+
 			out.println("연결성공 : " + conn + "<br>");			
 
-			JDBCUtility.commit(conn);
+			conn.commit();
 
-		}catch(Exception e){
+		}catch(SQLException e){
 
 			out.println("연결실패 : " + e.getMessage() + "<br>");			
 
@@ -40,7 +44,7 @@
 
 		}finally{
 
-			JDBCUtility.close(conn,null,null);
+			JDBCUtility.close(conn);
 
 		}
 
--- ddack/src/main/java/db/JDBCUtility.java
+++ ddack/src/main/java/db/JDBCUtility.java
@@ -7,40 +7,96 @@ import java.sql.Statement;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.sql.DataSource;
 
 public class JDBCUtility {
 
-	public static Connection getConnection(){
-		
-		Connection conn= null;
-			try {
-				Context initCtx = new InitialContext();
-				Context envCtx =(Context) initCtx.lookup("java:comp/env");
-				DataSource ds = (DataSource) envCtx.lookup("jdbc/ddack");
-				conn= ds.getConnection();
-				conn.setAutoCommit(false);
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-			return conn;
+	public static Connection getConnection() {
+
+		Context initContext;
+
+		Connection conn = null;
+
+		try {
+
+			initContext = new InitialContext();
+
+			Context envContext  = (Context)initContext.lookup("java:/comp/env");
+
+			DataSource ds = (DataSource)envContext.lookup("jdbc/ddack");
+
+			conn = ds.getConnection();
+
+		} catch (NamingException e) {
+
+			e.printStackTrace();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+		return conn;
+
 	}
 
-	public static void close(Connection conn,Statement stmt,ResultSet rs) {
+	
+
+	public static void close(ResultSet rs) {
+
 		try {
-			if(rs!=null) rs.close();
-			if(stmt!=null) stmt.close();
-			if(conn!=null) rs.close();
-		}catch(Exception e) {
-			//dummy
+
+			if (rs != null)	rs.close();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
 		}
+
 	}
+
+	public static void close(Statement stmt) {
+
+		try {
+
+			if (stmt != null)	stmt.close();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public static void close(Connection conn) {
+
+		try {
+
+			if (conn != null)	conn.close();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
 	public static void rollback(Connection conn) {
 
-		try {if(conn!=null) conn.rollback(); } catch(SQLException e) {}
-	}
-	public static void commit(Connection conn) {
-		
-		try {if(conn!=null) conn.commit(); } catch(SQLException e) {}
+		try {
+
+			if (conn != null)	conn.rollback();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
+		}
+
 	}
 }
